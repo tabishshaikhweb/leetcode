fc368562c0b80626602e19f9b9bc480aadcb34eb
tabishshaikhweb <tabishshaikhmum@gmail.com>
2026-09-17 23:46:34 +0530

    Time: 0 ms (100.00%) | Memory: 42.1 MB (6.16%) - LeetSync

--- 169-majority-element/majority-element.cpp
+++ 169-majority-element/majority-element.cpp
@@ -9,19 +9,5 @@ public:
             else c--;
         }
         return cand;
-        /*int i,c=0;
-        sort(nums.begin(),nums.end());
-        for(i=0;i<nums.size();i++)
-        {
-            c=0;
-            int x=nums[i];
-            for(int j=i;j<nums.size();j++)
-            {
-                if(x==nums[j]) c++;
-            }
-            if(c>nums.size()/2) break;
-        }
-        return nums[i];
-        */
     }
 };
